f61ad94caa546e9e6a8410cf03e3b560e94f10a5
Bruno Campos <brunocampos01@gmail.com>
2021-12-04 15:37:53 -0300

    Update create_averages.py

--- scripts/create_averages.py
+++ scripts/create_averages.py
@@ -2,17 +2,16 @@ import pandas as pd
 import numpy as np
 from RetailChurnTemplateUtility import *
 
-def azureml_main(dataframe1 = None, dataframe2 = None):
-    key_column='UserId'
-    IsDevelopment=True
+def azureml_main(df1=None, df2=None):
+    key_column = 'UserId'
+    IsDevelopment = True
 
     # Feature Engineering
-    churnUtil=RetailChurnTemplateUtility()
-    df=churnUtil.calculateAverages(dataframe1,
-                                   dataframe2,
-                                   key_column,
-                                   uniquable_columns=dataframe2.columns, 
-                                   summable_columns=dataframe1.columns)
+    churnUtil = RetailChurnTemplateUtility()
     
     # Return value must be of a sequence of pandas.DataFrame
-    return df
+    return churnUtil.calculateAverages(df1,
+                                       df2,
+                                       key_column,
+                                       uniquable_columns=df2.columns, 
+                                       summable_columns=df1.columns)
