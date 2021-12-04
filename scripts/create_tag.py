74a91e8af35e2afacd72844435519c5409a9aba4
Bruno Campos <brunocampos01@gmail.com>
2021-12-04 15:45:09 -0300

    Update create_tag.py

--- scripts/create_tag.py
+++ scripts/create_tag.py
@@ -21,7 +21,8 @@ def azureml_main(dataframe1 = None, dataframe2 = None):
     churn_period = 21
     churn_threshold = 0
   
-    dataframe1['Timestamp'] = dataframe1.apply(lambda x : dt.datetime.fromtimestamp(x['Timestamp']).strftime('%Y-%m-%d'), axis=1)
+    dataframe1['Timestamp'] = dataframe1\
+        .apply(lambda x : dt.datetime.fromtimestamp(x['Timestamp']).strftime('%Y-%m-%d'), axis=1)
     
     # Convert Column
     dataframe1['Parsed_Date'] = pd.to_datetime(dataframe1['Timestamp'])
@@ -29,16 +30,16 @@ def azureml_main(dataframe1 = None, dataframe2 = None):
     # Assigning Churn Status
     churnUtil = RetailChurnTemplateUtility(churn_period,churn_threshold)
     outdataframe = churnUtil.assign_churn_status(dataframe1,
-                                               key_column=key_column,
-                                               activity_column=activity_column) 
+                                                 key_column=key_column,
+                                                 activity_column=activity_column) 
     print('outdataframe.head()', outdataframe.head())
     print('outdataframe.dtypes\n', outdataframe.dtypes)
     
     # Missing values
-    outdataframe.fillna('Unknown', inplace=True)
+    outdataframe.fillna('Unknown', inplace = True)
     outdataframe['churn_period'] = churn_period
     
-    to_keep_list = [row for row in outdataframe.columns if row!='Parsed_Date']
+    to_keep_list = [row for row in outdataframe.columns if row != 'Parsed_Date']
     
     # Return value must be of a sequence of pandas.DataFrame
     return outdataframe[to_keep_list]
